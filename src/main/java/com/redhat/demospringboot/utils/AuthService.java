345620151afe45e4df3ad6de5a95e4e524422aa2
ws <wohshon@gmail.com>
2019-07-29 11:11:05 +0800

    updated auth services

--- src/main/java/com/redhat/demospringboot/utils/AuthService.java
+++ src/main/java/com/redhat/demospringboot/utils/AuthService.java
@@ -1,12 +1,29 @@
 package com.redhat.demospringboot.utils;
 
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class AuthService {
-
+    @Autowired
+    private Environment env;
+	Logger log=Logger.getLogger(this.getClass().getName());
+    
 	private String getPublicKey() {
-		return "";
+		
+		RestTemplate restTemplate = new RestTemplate();
+        final String uri = env.getProperty("sso.internal.endpoint");
+        log.info("uri: "+uri);
+		
+		ResponseEntity<String> response
+		  = restTemplate.getForEntity(uri + "/", String.class);
+		log.info(response.getBody());
+		return response.getBody();
 	}
 	
 	public String getInfo(String token) {
