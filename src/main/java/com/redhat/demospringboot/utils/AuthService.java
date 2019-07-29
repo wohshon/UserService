326e107493e27f396edeb57610d83793a86c64c2
ws <wohshon@gmail.com>
2019-07-29 13:13:12 +0800

    endpoint

--- src/main/java/com/redhat/demospringboot/utils/AuthService.java
+++ src/main/java/com/redhat/demospringboot/utils/AuthService.java
@@ -2,9 +2,13 @@ package com.redhat.demospringboot.utils;
 
 import java.util.logging.Logger;
 
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,8 +19,14 @@ public class AuthService {
 	Logger log=Logger.getLogger(this.getClass().getName());
     
 	private String getPublicKey() {
-		
-		RestTemplate restTemplate = new RestTemplate();
+		   CloseableHttpClient httpClient
+		      = HttpClients.custom()
+		        .setSSLHostnameVerifier(new NoopHostnameVerifier())
+		        .build();
+		    HttpComponentsClientHttpRequestFactory requestFactory 
+		      = new HttpComponentsClientHttpRequestFactory();
+		    requestFactory.setHttpClient(httpClient);
+		RestTemplate restTemplate = new RestTemplate(requestFactory);
         final String uri = env.getProperty("sso.internal.endpoint");
         log.info("uri: "+uri);
 		
